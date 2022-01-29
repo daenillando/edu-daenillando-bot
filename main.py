9ecea0a9cf84a580e9b6cbfd32fde349b406150a
daenillando <daenur.al@gmail.com>
2022-01-29 19:41:27 +0500

    Implemented /add /remove /list command for control SQL database (sqlite)

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import telebot
+from sql import database
 
 token = open('token.txt').readline().rstrip()
 bot = telebot.TeleBot(token)
+db = database("sql_database.db")
 
 # /start
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands = ["start"])
 def start(message):
-    bot.send_message(message.chat.id, "У аппарата ...")
+    bot.send_message(message.chat.id, "У аппарата ...", reply_markup = None)
+
+@bot.message_handler(commands = ["add"])
+def add(message):
+    category, name, price = message.text.split(' ')[1:4]
+    db.add(category, name, price)
+
+@bot.message_handler(commands = ["remove"])
+def remove(message):
+    id = message.text.split(' ')[1]
+    db.remove(id)
+
+@bot.message_handler(commands = ["list"])
+def list(message):
+    text = "id\tdate\tcategory\tname\tprice\n"
+    for row in db.list():
+        for field in row:
+            text += str(field) + "\t"
+        text += "\n"
+    bot.send_message(message.chat.id, text)
 
 # Return message back
-@bot.message_handler(content_types=["text"])
+@bot.message_handler(content_types = ["text"])
 def handle_text(message):
     bot.send_message(message.chat.id, "Echo: " + message.text)
 
---
+++ sql/sql.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+class database():
+    def __init__(self, filename):
+        self.filename = filename
+        con = sqlite3.connect(self.filename)
+        cur = con.cursor()
+        cur.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='purchases'")
+        if not cur.fetchone()[0] == 1:
+            cur.execute("CREATE TABLE purchases (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, category TEXT, name TEXT, price REAL)")
+        con.commit()
+        con.close()
+
+    def add(self, category, name, price):
+        con = sqlite3.connect(self.filename)
+        cur = con.cursor()
+        #print("INSERT INTO purchases(date, category, name, price) VALUES (CURRENT_DATE, {0}, {1}, {2})".format(category, name, price))
+        cur.execute("INSERT INTO purchases (date, category, name, price) VALUES (CURRENT_DATE, '{0}', '{1}', {2})".format(category, name, price))
+        con.commit()
+        con.close()
+
+    def remove(self, id):
+        con = sqlite3.connect(self.filename)
+        cur = con.cursor()
+        cur.execute("DELETE FROM purchases WHERE id = {0}".format(id))
+        con.commit()
+        con.close()
+
+    def list(self, category = None):
+        con = sqlite3.connect(self.filename)
+        cur = con.cursor()
+        list = []
+        for row in cur.execute("SELECT * FROM purchases ORDER BY date"):
+            list.append(row)
+        con.close()
+        return list
+
+    def sum(self, start_date, end_date, category = None):
+        pass
